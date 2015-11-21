f8a4ecd600b0dc337dce2ce7424e138fef21df4c
rpinsler <robertpinsler@live.de>
2015-11-21 19:35:51 +0100

    Adds downsampling script

---
+++ downsampleWav.py
@@ -0,0 +1,46 @@
+# taken from http://stackoverflow.com/questions/30619740/python-downsampling-wav-audio-file
+
+import wave
+import audioop
+
+def downsampleWav(src, dst, inrate=44100, outrate=16000, inchannels=2, outchannels=1):
+    if not os.path.exists(src):
+        print 'Source not found!'
+        return False
+
+    if not os.path.exists(os.path.dirname(dst)):
+        os.makedirs(os.path.dirname(dst))
+
+    try:
+        s_read = wave.open(src, 'r')
+        s_write = wave.open(dst, 'w')
+    except:
+        print 'Failed to open files!'
+        return False
+
+    n_frames = s_read.getnframes()
+    data = s_read.readframes(n_frames)
+
+    try:
+        converted = audioop.ratecv(data, 2, inchannels, inrate, outrate, None)
+        if outchannels == 1 & inchannels != 1:
+            converted[0] = audioop.tomono(converted[0], 2, 1, 0)
+    except:
+        print 'Failed to downsample wav'
+        return False
+
+    try:
+        s_write.setparams((outchannels, 2, outrate, 0, 'NONE', 'Uncompressed'))
+        s_write.writeframes(converted[0])
+    except:
+        print 'Failed to write wav'
+        return False
+
+    try:
+        s_read.close()
+        s_write.close()
+    except:
+        print 'Failed to close wav files'
+        return False
+
+    return True
